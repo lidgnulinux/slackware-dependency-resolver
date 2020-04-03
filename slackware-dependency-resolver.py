3bf3c3e1e7540098e45000959b50d3e69207a5f4
Ravexina <ravexina@gmail.com>
2020-04-03 20:41:30 +0430

    WARRANTY

--- slackware-dependency-resolver.py
+++ slackware-dependency-resolver.py
@@ -11,9 +11,15 @@ Github: https://github.com/ravexina/slackware-dependency-resolver
 
 The script relies on a list of dependencies provided by salixos.org
 
-===============================
-=== COMES WITH NO GUARANTEE ===
-===============================
+==============================================================================
+THE SCRIPT IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+THE SOFTWARE.
+==============================================================================
 
 """
 
